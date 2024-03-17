1b74844b016e18bd13ba15cf786d948226f737e8
KonkenBonken <konrad05pettersson@gmail.com>
2024-03-17 16:20:17 +0100

    generate grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
-export default function Home() {
+const width = 100, height = 100;
+
+export default function App() {
   return (
     <main>
+      {Array.from({ length: height }).fill(0).map((_, y) => (
+        <div key={y}>
+          {Array.from({ length: width }).fill(0).map((_, x) => (
+            <div key={x} />
+          ))}
+        </div>
+      ))}
     </main>
   );
 }
