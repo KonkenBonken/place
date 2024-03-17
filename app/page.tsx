7a92902eead4057b3f71e213377f594fbc03cdbd
KonkenBonken <konrad05pettersson@gmail.com>
2024-03-17 17:19:23 +0100

    add color palette

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import hsl from 'hsl-to-hex';
+
 const width = 100, height = 100;
 
-const grid = Array.from({ length: height }, () => Array.from<number>({ length: width }).fill(0xFFFFFF));
+const colorPalette = [
+  hsl(0, 95, 80), hsl(60, 95, 80), hsl(120, 95, 80), hsl(180, 95, 80), hsl(240, 95, 80), hsl(300, 95, 80), hsl(0, 0, 100),
+  hsl(0, 85, 50), hsl(60, 85, 50), hsl(120, 85, 50), hsl(180, 85, 50), hsl(240, 85, 50), hsl(300, 85, 50), hsl(0, 0, 50),
+  hsl(0, 95, 20), hsl(60, 95, 20), hsl(120, 95, 20), hsl(180, 95, 20), hsl(240, 95, 20), hsl(300, 95, 20), hsl(0, 0, 0),
+];
+
+const grid = Array.from({ length: height }, () => Array.from<string>({ length: width }).fill('#ffffff'));
 
 export default function App() {
   return (
@@ -8,7 +16,7 @@ export default function App() {
       {grid.map((row, y) =>
         <div key={y}>{
           row.map((clr, x) =>
-            <div key={x} style={clr !== 0xFFFFFF ? { backgroundColor: '#' + clr.toString(16) } : {}} />
+            <div key={x} style={clr !== '#ffffff' ? { backgroundColor: clr } : {}} />
           )
         }</div>
       )}
