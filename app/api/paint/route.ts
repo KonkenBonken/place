cfe8a3983eae957ffdf3fd137a64e1a83caa6ba5
KonkenBonken <konrad05pettersson@gmail.com>
2024-03-18 23:11:03 +0100

    limit to one paint per 5min

--- app/api/paint/route.ts
+++ app/api/paint/route.ts
@@ -1,3 +1,5 @@
+import { cookies } from 'next/headers';
+
 import { paint } from '@/app/Grid';
 
 export async function POST(request: Request) {
@@ -6,7 +8,16 @@ export async function POST(request: Request) {
   if (typeof x !== 'number' || typeof y !== 'number' || typeof id !== 'string')
     return new Response(null, { status: 400 });
 
+  const cookieStore = cookies();
+
+  if (cookieStore.has('hasPainted'))
+    return new Response(null, { status: 401 });
+
   const success = paint(x, y, id);
 
-  return new Response(null, { status: success ? 200 : 400 });
+  cookieStore.set({ name: 'hasPainted', value: '', maxAge: 5 * 60 });
+
+  return new Response(null, {
+    status: success ? 200 : 400
+  });
 }
