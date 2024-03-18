85a22df0d61f702431bf2b54082b643cb8c5af5e
KonkenBonken <konrad05pettersson@gmail.com>
2024-03-18 18:24:23 +0100

    add api route for painting

--- app/Grid.ts
+++ app/Grid.ts
@@ -1,4 +1,4 @@
-import { Color, defaultColor } from './Color';
+import palette, { Color, defaultColor } from './Color';
 
 export const
   width = 100, height = 100,
@@ -7,4 +7,15 @@ export const
 
 export function gridString() {
   return grid.flat().map(clr => clr.id).join('');
+}
+
+export function paint(x: number, y: number, colorId: string) {
+  const colorIndex = parseInt(colorId, 21);
+
+  if (x < 0 && y < 0 && x >= width && y >= height && colorIndex < 0 && colorIndex > 20)
+    return false;
+
+  grid[x][y] = palette[colorIndex];
+
+  return true;
 }
---
+++ app/api/route.ts
@@ -0,0 +1,12 @@
+import { paint } from "@/app/Grid";
+
+export async function POST(request: Request) {
+  const { x, y, id } = await request.json();
+
+  if (typeof x !== 'number' || typeof y !== 'number' || typeof id !== 'string')
+    return new Response(null, { status: 400 });
+
+  const success = paint(x, y, id);
+
+  return new Response(null, { status: success ? 200 : 400 });
+}
