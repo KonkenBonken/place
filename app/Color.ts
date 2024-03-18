96ebc4edcb095899767ec43e3cc4f27820a7aabc
KonkenBonken <konrad05pettersson@gmail.com>
2024-03-18 17:48:49 +0100

    change color lightness

--- app/Color.ts
+++ app/Color.ts
@@ -19,9 +19,9 @@ export class Color {
 }
 
 const palette = [
-  new Color(0, 95, 80), new Color(60, 95, 80), new Color(120, 95, 80), new Color(180, 95, 80), new Color(240, 95, 80), new Color(300, 95, 80), new Color(0, 0, 100),
+  new Color(0, 95, 75), new Color(60, 95, 75), new Color(120, 95, 75), new Color(175, 95, 75), new Color(240, 95, 75), new Color(300, 95, 75), new Color(0, 0, 100),
   new Color(0, 85, 50), new Color(60, 85, 50), new Color(120, 85, 50), new Color(180, 85, 50), new Color(240, 85, 50), new Color(300, 85, 50), new Color(0, 0, 50),
-  new Color(0, 95, 20), new Color(60, 95, 20), new Color(120, 95, 20), new Color(180, 95, 20), new Color(240, 95, 20), new Color(300, 95, 20), new Color(0, 0, 0),
+  new Color(0, 95, 30), new Color(60, 95, 30), new Color(120, 95, 30), new Color(180, 95, 30), new Color(240, 95, 30), new Color(300, 95, 30), new Color(0, 0, 0),
 ];
 
 export default palette;
