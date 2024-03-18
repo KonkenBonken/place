c6c371b236493b35377aad10a04c321118e4a72a
KonkenBonken <konrad05pettersson@gmail.com>
2024-03-18 23:32:43 +0100

    display popup at rate limit

--- app/ColorPicker.tsx
+++ app/ColorPicker.tsx
@@ -1,19 +1,32 @@
+import { useState } from 'react';
+
 import palette from './Color';
 
 export default function ColorPicker({ selected, fetchGrid }: { selected: `${number}-${number}` | null, fetchGrid(): void }) {
+  const [painted, setPainted] = useState(false);
+
   if (!selected)
     return null;
 
   const [x, y] = selected.split('-').map(n => parseInt(n));
 
   return <section id="picker">
+    {painted && <section>You can only paint once every 5min</section>}
     {palette.map((clr, i) => <div
       key={i} style={clr.style}
       onClick={() =>
         fetch('/api/paint', {
           method: 'POST',
           body: JSON.stringify({ x, y, id: clr.id })
-        }).then(fetchGrid)
+        })
+          .then(res => {
+            switch (res.status) {
+              case 401:
+                setPainted(true);
+                setTimeout(() => setPainted(false), 5000);
+            }
+            fetchGrid();
+          })
       }
     />)}
   </section>;
