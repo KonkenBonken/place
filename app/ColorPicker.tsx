dbdf12ce7e39a9dde86ccec3acdda5d75bf8abfe
KonkenBonken <konrad05pettersson@gmail.com>
2024-03-18 23:41:31 +0100

    allow for touch screens

--- app/ColorPicker.tsx
+++ app/ColorPicker.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import palette from './Color';
+import palette, { type Color } from './Color';
 
 export default function ColorPicker({ selected, fetchGrid }: { selected: `${number}-${number}` | null, fetchGrid(): void }) {
   const [painted, setPainted] = useState(false);
@@ -10,24 +10,26 @@ export default function ColorPicker({ selected, fetchGrid }: { selected: `${numb
 
   const [x, y] = selected.split('-').map(n => parseInt(n));
 
+  const click = (clr: Color) => () =>
+    fetch('/api/paint', {
+      method: 'POST',
+      body: JSON.stringify({ x, y, id: clr.id })
+    })
+      .then(res => {
+        switch (res.status) {
+          case 401:
+            setPainted(true);
+            setTimeout(() => setPainted(false), 5000);
+        }
+        fetchGrid();
+      });
+
   return <section id="picker">
     {painted && <section>You can only paint once every 5min</section>}
     {palette.map((clr, i) => <div
       key={i} style={clr.style}
-      onClick={() =>
-        fetch('/api/paint', {
-          method: 'POST',
-          body: JSON.stringify({ x, y, id: clr.id })
-        })
-          .then(res => {
-            switch (res.status) {
-              case 401:
-                setPainted(true);
-                setTimeout(() => setPainted(false), 5000);
-            }
-            fetchGrid();
-          })
-      }
+      onClick={click(clr)}
+      onTouchStart={click(clr)}
     />)}
   </section>;
 }
