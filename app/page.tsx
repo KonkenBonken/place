568db4cc1af03d211788c1d19398ae26054e5d71
KonkenBonken <konrad05pettersson@gmail.com>
2024-03-18 17:26:49 +0100

    allow selecting pixel

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useState } from 'react';
 import { Color, defaultColor } from './Color';
 
 const width = 100, height = 100;
@@ -5,12 +8,17 @@ const width = 100, height = 100;
 const grid = Array.from({ length: height }, () => Array.from<Color>({ length: width }).fill(defaultColor));
 
 export default function App() {
+  const [selected, setSelected] = useState<`${number}-${number}`>();
+
   return (
     <main>
       {grid.map((row, y) =>
         <div key={y}>{
           row.map((clr, x) =>
-            <div key={x} style={clr.style} />
+            <div key={x} style={clr.style}
+              className={`${x}-${y}` === selected ? 'selected' : undefined}
+              onClick={() => setSelected(`${x}-${y}`)}
+            />
           )
         }</div>
       )}
