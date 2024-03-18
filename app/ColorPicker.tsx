0742c23f127c6c03add392ff0fde6ce8ab84f12e
KonkenBonken <konrad05pettersson@gmail.com>
2024-03-18 18:24:55 +0100

    paint on color selection

--- app/ColorPicker.tsx
+++ app/ColorPicker.tsx
@@ -1,7 +1,20 @@
 import palette from './Color';
 
-export default function ColorPicker() {
+export default function ColorPicker({ selected }: { selected: `${number}-${number}` | null }) {
+  if (!selected)
+    return null;
+
+  const [x, y] = selected.split('-').map(n => parseInt(n));
+
   return <section id="picker">
-    {palette.map((clr, i) => <div key={i} style={clr.style} />)}
+    {palette.map((clr, i) => <div
+      key={i} style={clr.style}
+      onClick={() =>
+        fetch('/api', {
+          method: 'POST',
+          body: JSON.stringify({ x, y, id: clr.id })
+        })
+      }
+    />)}
   </section>;
 }
